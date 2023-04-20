35e2ad1a4308cd374aa1f8d12be75a6f1f714729
Michel <hltvexe@mail.ru>
2023-04-20 13:38:46 +0300

    реализованы 2 задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = b ** 2 - 4 * a * c;
+	let arr = [];
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	} if (d === 0) {
+		arr.push(-b / (2 * a));
+	};
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = Number(percent / 1200);
+	if (percent === NaN) {
+		return false;
+	}
+	let body = amount - contribution;
+	let monthPay = body * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+	return (monthPay * countMonths).toFixed(2);
 }
